1d735614e6f8f06750f24d0e9d168b77be5df580
mcra02 <maicolcrodrigoa@gmail.com>
2020-11-08 16:00:10 -0500

    updated cors

--- index.js
+++ index.js
@@ -10,15 +10,22 @@ dotenv.config()
 const whitelist = process.env.CORS_SITES.split(',')
 let app  =  express()
 
-let corsOptions = {
-    origin: function (origin, callback) {
-      if (!origin || whitelist.indexOf(origin) !== -1) {
-        callback(null, true)
-      } else {
-        callback(new Error('Not allowed by CORS'))
-      }
-    }
-  }
+// let corsOptions = {
+//     origin: function (origin, callback) {
+//       if (!origin || whitelist.indexOf(origin) !== -1) {
+//         callback(null, true)
+//       } else {
+//         callback(new Error('Not allowed by CORS'))
+//       }
+//     }
+//   }
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 // app.use(cors(corsOptions))
 app.use(morgan('dev'))
@@ -27,7 +34,7 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 
-app.post('/email/send', cors(corsOptions), async (req, res) => {
+app.post('/email/send', async (req, res) => {
     const status = {}
     try {
         await EmailService.sendEmail(req.body)
